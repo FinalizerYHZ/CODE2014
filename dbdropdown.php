e683b14a2ab0cf00e370aab69a323ed4155f38f3
FinalizerYHZ <jon.maclellan@smu.ca>
2014-03-01 18:32:42 -0400

    Added PHP sample file used to create a dropdown box populated with data from a MySQL DB.

---
+++ dbdropdown.php
@@ -0,0 +1,44 @@
+<?php
+// NOTE: For security purposes, the DB name, location and password were removed from this file.
+// They would normally go here.
+
+
+// Beneath is code to create a table of the SQL database and show the first 30 rows of information.
+// The tabbing is a little disjointed but the data can be shown without issue.
+// The Comments was removed because of the sheer size of the field and the resulting alterations
+// to the table made it unreadable.
+
+/*
+$query = "SELECT * FROM CODEMode3 LIMIT 30";
+$category = mysql_query($query) or die(mysql_error());
+
+$numRecords = mysql_num_rows($category); 
+$catCount = 0;
+$currentItem = "";
+
+echo "<table><tr><td><ul>";
+
+echo ("IndexCounter"."&nbsp; "."RecallNum"."&nbsp; "."Year"."&nbsp; "."Manufacturer"."&nbsp; "."VehicleCategory"."&nbsp; "."VehMake"."&nbsp; "."ModelName"."&nbsp; "."ModelName"."&nbsp; "."UnitsAffected"."&nbsp; "."System"."&nbsp; "."Notification"."</br>");
+
+for ($i = 0; $i < $numRecords; $i++) 
+{
+ $row = mysql_fetch_array($category);
+ echo ("$row[IndexCounter]"."&nbsp; "."$row[RecallNum]"."&nbsp; "."$row[Year]"."&nbsp; "."$row[Manufacturer]"."&nbsp; "."$row[VehicleCategory]"."&nbsp; "."$row[VehMake]"."&nbsp; "."$row[ModelName]"."&nbsp; "."$row[UnitsAffected]"."&nbsp; "."$row[System]"."&nbsp; "."$row[Notification]"."</br>");
+}
+*/
+
+
+// Below is code to create a dropdown box populated with certain pieces of information from the
+// MySQL database. In this case, we can show the first 30 different manufacturers from the database in
+// no distinct order.
+
+$sql = "SELECT distinct VehMake FROM CODEMode3 LIMIT 30";
+$result = mysql_query($sql) or die(mysql_error());
+
+echo "<select name='VehMake'>";
+while ($row = mysql_fetch_array($result)) {
+    echo "<option value='" . $row[VehMake] . "'>" . $row[VehMake] . "</option>";
+}
+echo "</select>";
+
+?>
