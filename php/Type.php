8aca5f29ff026fb42f8747dadf59f085568b5c64
FinalizerYHZ <jon.maclellan@smu.ca>
2014-03-02 15:53:10 -0400

    Added 4 PHP files for interaction with database.

---
+++ php/Type.php
@@ -0,0 +1,19 @@
+<?php
+$db_location = "localhost"; 
+$db_username = "cs4477215";
+$db_password = "finPelk2";
+$db_database = "cs4477215";
+$db_connection = mysql_connect("$db_location","$db_username","$db_password") 
+	or die ("Error - Could not connect to MySQL Server");
+$db = mysql_select_db($db_database,$db_connection) 
+	or die ("Error - Could not open database");
+
+$arr="";
+$sql = "SELECT distinct VehicleCategory FROM CODECleaned ORDER BY VehicleCategory ASC";
+$result = mysql_query($sql) or die(mysql_error());
+while ($row = mysql_fetch_array($result)) {
+    $arr = $arr.$row[VehicleCategory].",";
+}
+echo $arr;
+
+?>
