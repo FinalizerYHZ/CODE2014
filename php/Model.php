f25deb6c9af56eb4b64a2c4f771ce8f0b79e9017
FinalizerYHZ <jon.maclellan@smu.ca>
2014-03-02 17:14:00 -0400

    Updated PHP files to use new server's credentials.

--- php/Model.php
+++ php/Model.php
@@ -1,8 +1,8 @@
 <?php
-$db_location = "cs.smu.ca"; 
-$db_username = "cs4477215";
-$db_password = "finPelk2";
-$db_database = "cs4477215";
+$db_location = "localhost"; 
+$db_username = "root";
+$db_password = "password";
+$db_database = "CODE2014";
 $db_connection = mysql_connect("$db_location","$db_username","$db_password") 
 	or die ("Error - Could not connect to MySQL Server");
 $db = mysql_select_db($db_database,$db_connection) 
